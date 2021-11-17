581b3547e3757ba94acd52d59e874e1231ce6d71
evchambers <e.v.chambers@sheffield.ac.uk>
2021-11-17 20:50:14 +0000

    Add the pvals and freq data

--- app.R
+++ app.R
@@ -1,17 +1,17 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This is MetSurvCurv
+# 
+
 
 library(shiny)
-library(tidyverse)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(stringr)
 library(ggfortify)
 library(partykit)
 library(survival)
+library(ggpmisc)
 library(coin)
 
 #data read in
@@ -62,23 +62,27 @@ server <- function(input, output) {
         fmula = as.formula(paste("surv_xfs ~ ", GName))
         ctree_xfs <- ctree(fmula, data = curve_data)
         splitrules <- partykit:::.list.rules.party(ctree_xfs, i=nodeids(ctree_xfs))[2] %>% str_split(pattern = " ")
-        split <- as.numeric(splitrules[[1]][3])
+        split <- round(as.numeric(splitrules[[1]][3]),3)
+        curve_data$split <- split
         #signif <- 1 - ctree_xfs@tree$criterion$maxcriterion
+        curve_data <- curve_data %>% mutate(threshold = ifelse(get(GName)<=split, paste(input$genename,intToUtf8(8804) , split), paste(input$genename,">", split)))
         
-        curve_data <- curve_data %>% mutate(threshold = get(GName)<=split)
 
         curve_data
     })
     observe({print(curve_data())})
     output$survPlot <- renderPlot({
-            surv_xfs_dat <- survfit(surv_xfs~threshold,curve_data())
+            plot_data <- curve_data()
+            surv_xfs_dat <- survfit(surv_xfs~threshold,plot_data)
+            surv_diff_dat <- survdiff(surv_xfs~threshold,plot_data)
+            freqs <- plot_data %>% count(threshold)
+            newPval <- signif(pchisq(surv_diff_dat$chisq, df = length(surv_diff_dat$n)-1, lower.tail=FALSE),3)
             autoplot(surv_xfs_dat) +
-            labs(x = "\n Survival Time (Days) ", y = "Survival Probabilities \n", 
-                 title = input$genename) + 
-            theme(plot.title = element_text(hjust = 0.5), 
-                  axis.title.x = element_text(face="bold", colour="#FF7A33", size = 12),
-                  axis.title.y = element_text(face="bold", colour="#FF7A33", size = 12),
-                  legend.title = element_text(face="bold", size = 10))
+                labs(x = "\n Time to Metastasis", y = "Probability of freedom from metastasis\n", title = paste(input$genename,"- p=", newPval)) +
+                theme(plot.title = element_text(hjust = 0.5))+
+                annotate(geom = "table", x = 20, y = 0.2, label = list(freqs), 
+                     vjust = 1, hjust = 0) +
+                ylim(0,1)
     })
 }
 
